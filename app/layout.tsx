02ea433d59fa956c545bc9e2e04b55cc0d92b1c0
Michal Fasanek <misa.fasa@gmail.com>
2025-08-06 16:36:47 +0200

    add css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "LaunchDarkly OAuth Framework",
+    description: "A framework for handling OAuth authentication with LaunchDarkly services.",
+};
 
 export default function RootLayout({
     children,
-}: {
+}: Readonly<{
     children: React.ReactNode
-}) {
+}>) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body suppressHydrationWarning={true}>{children}</body>
         </html>
     );
 }
